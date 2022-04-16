2563291ab6efe5b20e4f5ecd9e1da33bd8a6f01c
Web-Eye <Web-Eye@gmx.net>
2022-04-16 17:14:00 +0200

    bugfix MariaDB 1442 error

--- libs/core/Datalayer/DL_items.py
+++ libs/core/Datalayer/DL_items.py
@@ -59,11 +59,15 @@ class DL_items:
 
     @staticmethod
     def deleteExpiredItems(con, project):
-        statement = 'DELETE FROM items WHERE item_id IN (' \
+        statement = 'UPDATE items SET identifier = -1 WHERE item_id IN (' \
                     '   SELECT i.item_id FROM items  AS i' \
                     '   LEFT JOIN sub_items AS si ON i.item_id = si.item_id ' \
-                    '   WHERE project = ? AND si.availableTo_date < NOW()' \
+                    '   WHERE project = ? AND si.availableTo_date IS NOT Null AND si.availableTo_date < NOW()' \
                     ');'
+
+        databaseHelper.executeNonQuery(con, statement, (project,))
+
+        statement = 'DELETE FROM items WHERE project = ? AND identifier = -1;'
 
         row_count, _id = databaseHelper.executeNonQuery(con, statement, (project,))
         return row_count
