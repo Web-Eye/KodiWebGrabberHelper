d112ca15c1de28d11e706e88fcccce923872bfe1
Web-Eye <Web-Eye@gmx.net>
2022-04-19 15:46:48 +0200

    bump to 1.1.0+beta

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from libs.core.databaseCore import databaseCore
 from libs.core.hdtrailersCore import hdtrailersCore
 from libs.common.tools import GetPIDFile, GetConfigFile, ReadConfig, SaveConfig
 
-__VERSION__ = '1.0.0+Beta'
+__VERSION__ = '1.1.0+Beta'
 __VERSIONSTRING__ = f'KodiWebGrabberHelper Version {__VERSION__}'
 
 
