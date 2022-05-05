3470fa6b8fe95a05c8cff2e7bfd072a34f4b9ee8
Web-Eye <web-eye@gmx.net>
2022-05-05 16:12:37 +0200

    new database structure

--- KodiWebGrabber/libs/core/databaseCore.py
+++ KodiWebGrabber/libs/core/databaseCore.py
@@ -7,7 +7,7 @@ from .databaseHelper import databaseHelper
 class databaseCore:
 
     CURRENT_DB_VERSION = 1
-    DB_NAME = 'KodiWebGrabber'
+    DB_NAME = 'KodiWebGrabber_Test'
 
     def __init__(self, config):
         self._config = config
@@ -60,15 +60,73 @@ class databaseCore:
 
         if dbVersion == 0:
 
+            statement = 'CREATE TABLE IF NOT EXISTS projects (' \
+                        '   project_id INT NOT NULL AUTO_INCREMENT,' \
+                        '   name VARCHAR(40) NOT NULL,' \
+                        '   PRIMARY KEY (`project_id`)' \
+                        ');'
+
+            databaseHelper.executeNonQuery(con, statement)
+
+            statement = 'CREATE TABLE IF NOT EXISTS item_tags (' \
+                        '   tag_id INT NOT NULL AUTO_INCREMENT,' \
+                        '   name VARCHAR(40) NOT NULL,' \
+                        '   PRIMARY KEY (`tag_id`)' \
+                        ');'
+
+            databaseHelper.executeNonQuery(con, statement)
+
+            statement = 'INSERT INTO item_tags (name) VALUES (\'NONE\');'
+            databaseHelper.executeNonQuery(con, statement)
+
+            statement = 'CREATE TABLE IF NOT EXISTS qualities (' \
+                        '   quality_id INT NOT NULL AUTO_INCREMENT,' \
+                        '   name VARCHAR(40) NOT NULL,' \
+                        '   PRIMARY KEY (`quality_id`)' \
+                        ');'
+
+            databaseHelper.executeNonQuery(con, statement)
+
+            statement = 'INSERT INTO qualities (name) VALUES (\'270p\');'
+            databaseHelper.executeNonQuery(con, statement)
+            statement = 'INSERT INTO qualities (name) VALUES (\'360p\');'
+            databaseHelper.executeNonQuery(con, statement)
+            statement = 'INSERT INTO qualities (name) VALUES (\'480p\');'
+            databaseHelper.executeNonQuery(con, statement)
+            statement = 'INSERT INTO qualities (name) VALUES (\'540p\');'
+            databaseHelper.executeNonQuery(con, statement)
+            statement = 'INSERT INTO qualities (name) VALUES (\'720p\');'
+            databaseHelper.executeNonQuery(con, statement)
+            statement = 'INSERT INTO qualities (name) VALUES (\'1080p\');'
+            databaseHelper.executeNonQuery(con, statement)
+
+            statement = 'CREATE TABLE IF NOT EXISTS subitem_tags (' \
+                        '   tag_id INT NOT NULL AUTO_INCREMENT,' \
+                        '   name VARCHAR(40) NOT NULL,' \
+                        '   PRIMARY KEY (`tag_id`)' \
+                        ');'
+
+            databaseHelper.executeNonQuery(con, statement)
+
+            statement = 'INSERT INTO subitem_tags (name) VALUES (\'NONE\');'
+            databaseHelper.executeNonQuery(con, statement)
+
+            statement = 'CREATE TABLE IF NOT EXISTS list_identifier (' \
+                        '   identifier_id INT NOT NULL AUTO_INCREMENT,' \
+                        '   name VARCHAR(40) NOT NULL,' \
+                        '   PRIMARY KEY (`identifier_id`)' \
+                        ');'
+
+            databaseHelper.executeNonQuery(con, statement)
+
             statement = 'CREATE TABLE IF NOT EXISTS items (' \
                         '   item_id INT NOT NULL AUTO_INCREMENT,' \
-                        '   project ENUM(\'HARTABERFAIR\', \'INASNACHT\', \'ROCKPALAST\', \'HDTRAILERS\') NOT NULL,' \
+                        '   project_id INT NOT NULL,' \
                         '   identifier VARCHAR(128) NOT NULL,' \
                         '   hash BINARY(16),' \
                         '   title VARCHAR(255) NOT NULL,' \
                         '   plot VARCHAR(4096),' \
-                        '   tag ENUM(\'NONE\', \'SIGNLANGUAGE\', \'MUSICCLIP\', \'INTERVIEW\', \'UNPLUGGED\', ' \
-                        '   \'LIVEPREVIEW\') NOT NULL,' \
+                        '   tag_id INT NOT NULL,' \
                         '   poster_url VARCHAR(255),' \
                         '   order_date DATETIME NOT NULL,' \
                         '   PRIMARY KEY (`item_id`)' \
@@ -76,11 +134,16 @@ class databaseCore:
 
             databaseHelper.executeNonQuery(con, statement)
 
+            statement = 'CREATE INDEX idxIproject_id ON sub_items (project_id);'
+            databaseHelper.executeNonQuery(con, statement)
+            statement = 'CREATE INDEX idxItag_id ON sub_items (tag_id);'
+            databaseHelper.executeNonQuery(con, statement)
+
             statement = 'CREATE TABLE IF NOT EXISTS sub_items (' \
                         '   subItem_id INT NOT NULL AUTO_INCREMENT,' \
                         '   item_id INT NOT NULL,' \
                         '   title VARCHAR(255),' \
-                        '   tag ENUM (\'NONE\', \'TRAILER\', \'TEASER\', \'CLIP\') NOT NULL,' \
+                        '   tag_id INT NOT NULL,' \
                         '   broadcastOn_date DATETIME,' \
                         '   availableTo_date DATETIME ,' \
                         '   duration INT,' \
@@ -91,11 +154,13 @@ class databaseCore:
 
             statement = 'CREATE INDEX idxSIitem_id ON sub_items (item_id);'
             databaseHelper.executeNonQuery(con, statement)
+            statement = 'CREATE INDEX idxSItag_id ON sub_items (tag_id);'
+            databaseHelper.executeNonQuery(con, statement)
 
             statement = 'CREATE TABLE IF NOT EXISTS links (' \
                         '   link_id INT NOT NULL AUTO_INCREMENT,' \
                         '   subItem_id INT NOT NULL,' \
-                        '   quality ENUM (\'270p\', \'360p\', \'480p\', \'540p\', \'720p\', \'1080p\') NOT NULL,' \
+                        '   quality_id INT NOT NULL,' \
                         '   best_quality BIT,' \
                         '   hoster VARCHAR(64),' \
                         '   size INT,' \
@@ -105,13 +170,14 @@ class databaseCore:
 
             databaseHelper.executeNonQuery(con, statement)
 
-            statement = 'CREATE INDEX idxLsubItem_id ON links (subItem_id);'
+            statement = 'CREATE INDEX idxLSTsubItem_id ON links (subItem_id);'
+            databaseHelper.executeNonQuery(con, statement)
+            statement = 'CREATE INDEX idxLSTquality_id ON links (quality_id);'
             databaseHelper.executeNonQuery(con, statement)
 
             statement = 'CREATE TABLE IF NOT EXISTS lists (' \
                         '   list_id INT NOT NULL AUTO_INCREMENT,' \
-                        '   identifier ENUM(\'HDT_MOSTWATCHEDWEEK\', \'HDT_MOSTWATCHEDTODAY\', \'HDT_TOPTEN\', ' \
-                        '   \'HDT_OPENING\', \'HDT_COMINGSOON\') NOT NULL,' \
+                        '   identifier_id INT NOT NULL,' \
                         '   item_id INT NOT NULL,' \
                         '   order_id INT NOT NULL,' \
                         '   PRIMARY KEY (`list_id`)' \
@@ -119,7 +185,9 @@ class databaseCore:
 
             databaseHelper.executeNonQuery(con, statement)
 
-            statement = 'CREATE INDEX idxLitem_id ON lists (item_id);'
+            statement = 'CREATE INDEX idxLNKidentifier_id ON links (identifier_id);'
+            databaseHelper.executeNonQuery(con, statement)
+            statement = 'CREATE INDEX idxLNKitem_id ON lists (item_id);'
             databaseHelper.executeNonQuery(con, statement)
 
             statement = 'CREATE TABLE IF NOT EXISTS settings (' \
@@ -133,12 +201,27 @@ class databaseCore:
 
             statement = 'CREATE VIEW viewItems AS' \
                         '   SELECT ' \
-                        '      i.item_id, i.project, i.title, i.plot, i.tag, i.poster_url, si.subitem_id, ' \
-                        '      si.si.title AS si_title, si.tag AS si_tag, si.broadcastOn_date, si.availableTo_date, ' \
-                        '      si.duration, li.link_id, li.quality, li.best_quality, li.hoster, li.size, li.url  ' \
+                        '      i.item_id, p.name as project, i.title, i.plot, it.name AS tag, i.poster_url ' \
                         '   FROM items AS i' \
+                        '   LEFT JOIN projects AS p ON i.project_id = p.project_id' \
+                        '   LEFT JOIN item_tags AS it ON i.tag_id = it.tag_id' \
+                        '   ORDER BY i.order_date DESC;'
+
+            databaseHelper.executeNonQuery(con, statement)
+
+            statement = 'CREATE VIEW viewItemLinks AS' \
+                        '   SELECT ' \
+                        '      i.item_id, p.name as project, i.title, i.plot, it.name AS tag, i.poster_url, ' \
+                        '      si.subitem_id, si.title AS si_title, sit.name AS si_tag, si.broadcastOn_date, ' \
+                        '      si.availableTo_date, si.duration, li.link_id, q.name AS quality, li.best_quality, ' \
+                        '      li.hoster, li.size, li.url  ' \
+                        '   FROM items AS i' \
+                        '   LEFT JOIN projects AS p ON i.project_id = p.project_id' \
+                        '   LEFT JOIN item_tags AS it ON i.tag_id = it.tag_id' \
                         '   LEFT JOIN sub_items AS si ON i.item_id = si.item_id' \
+                        '   LEFT JOIN subitem_tags AS sit ON si.tag_id = sit.tag_id' \
                         '   LEFT JOIN links AS li ON si.subItem_id = li.subItem_id' \
+                        '   LEFT JOIN qualities AS q ON li.quality_id = q.quality_id' \
                         '' \
                         '   ORDER BY i.order_date DESC, si.broadcastOn_date DESC;'
 
