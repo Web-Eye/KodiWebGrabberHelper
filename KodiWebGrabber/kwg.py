753cc69998e44d16f8dcb6b1b4cecf5250a4d1b8
WebEye <web-eye@gmx.net>
2024-12-16 16:28:15 +0100

    bump version

--- KodiWebGrabber/kwg.py
+++ KodiWebGrabber/kwg.py
@@ -27,7 +27,7 @@ from libs.common.PIDhandler import PIDhandler
 from libs.core.databaseCore import databaseCore
 from libs.common.tools import GetPIDFile, GetConfigFile, ReadConfig, SaveConfig
 
-__VERSION__ = '1.4.0'
+__VERSION__ = '1.4.1'
 __VERSIONSTRING__ = f'KodiWebGrabberHelper Version {__VERSION__}'
 
 
